42f57c7eb520e3becb25a3920bdf7a00238779ec
Arven Lagawan <arvenlagawan0731@gmail.com>
2026-02-15 00:09:27 +0800

    added configs

--- src/core/config.py
+++ src/core/config.py
@@ -1,12 +1,39 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
-import logging,sys
+from pathlib import Path
+from dotenv import load_dotenv
+import logging,sys,os
 
-#LIMITER CONFIG 
+#=================================
+        #LIMITER CONFIG
+#=================================
 limiter = Limiter(key_func=get_remote_address)
 
-#LOGGING CONFIG 
+#=================================
+        #LOGGING CONFIG
+#=================================
 LOG_FORMAT = "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler(sys.stdout)], format=LOG_FORMAT)
+
+#=================================
+        #ENVIRONMENT CONFIG
+#=================================
+load_dotenv()
+
+ENV = os.getenv('ENVIRONMENT','local')
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+env_file_map = {
+    "local":".env.local",
+    "docker":".env.docker"
+}
+
+env_file = env_file_map.get(ENV)
+
+if env_file:
+    env_path = BASE_DIR / env_file
+    if env_path.exists():
+        load_dotenv(env_path)
+
