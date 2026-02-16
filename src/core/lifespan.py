f7deec5182d876649f5c4a2369e31d225801fea7
Arven Lagawan <arvenlagawan0731@gmail.com>
2026-02-16 19:00:02 +0800

    added db initialization

--- src/core/lifespan.py
+++ src/core/lifespan.py
@@ -2,14 +2,24 @@ from fastapi import FastAPI,Request,Response
 from slowapi.errors import RateLimitExceeded
 from contextlib import asynccontextmanager
 
-import logging
+from src.core.config import ENV
+from src.database.db_config import init_db
+
+import logging,os
 
 logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
 
-    
+    #===============================
+        #INITIALIZE DATABASE
+    #===============================
+    if os.getenv("TESTING") == "1":
+        logger.info("Running in TESTING mode")
+    elif os.getenv("ENVIRONMENT") == "local" or "docker":
+        init_db()
+
     logger.info('[SERVER] Application Startup Complete')
     yield
     logger.info('[SERVER] Application Shutdown Complete')
